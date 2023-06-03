0f353c4736a5fa868f2a2fd9da689e25ef44d3bc
saint6839 <saint6839@gmail.com>
2023-06-03 20:14:35 +0900

    OVG-6 feat : initial settings

--- src/main/java/jungle/ovengers/controller/MemberController.java
+++ src/main/java/jungle/ovengers/controller/MemberController.java
@@ -1,6 +1,7 @@
 package jungle.ovengers.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
