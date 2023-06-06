9de8f45c53b11d47146ad6b8bf363e6afad895fe
saint6839 <saint6839@gmail.com>
2023-06-06 14:37:32 +0900

    feat : health check 테스트용 메서드 추가

--- src/main/java/jungle/ovengers/controller/MemberController.java
+++ src/main/java/jungle/ovengers/controller/MemberController.java
@@ -12,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
+
+    @GetMapping("/")
+    public String home() {
+        return "success";
+    }
+
     @GetMapping("/oauth/kakao")
     public void test(@RequestParam String code) {
         System.out.println("테스트 : " + code);
