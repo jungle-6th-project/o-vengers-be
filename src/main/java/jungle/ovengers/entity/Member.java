7974dfc64dcfafb52c57ae5e51148a1f44e170db
saint6839 <saint6839@gmail.com>
2023-06-08 23:32:02 +0900

    feat : member entity 추가

---
+++ src/main/java/jungle/ovengers/entity/Member.java
@@ -0,0 +1,27 @@
+package jungle.ovengers.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String profile;
+}
