c967bc95912031943308042414aa37c37ec977ca
saint6839 <saint6839@gmail.com>
2023-06-10 00:22:40 +0900

    OVG-22 refactor : 변경된 response format에 맞도록 변경

--- src/main/java/jungle/ovengers/controller/MemberController.java
+++ src/main/java/jungle/ovengers/controller/MemberController.java
@@ -2,9 +2,11 @@ package jungle.ovengers.controller;
 
 import jungle.ovengers.model.response.Token;
 import jungle.ovengers.service.MemberService;
+import jungle.ovengers.support.ApiResponse;
+import jungle.ovengers.support.ApiResponseGenerator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,7 +24,7 @@ public class MemberController {
     }
 
     @PostMapping("/tokens")
-    public ResponseEntity<Token> publishToken(@RequestParam String authCode) {
-        return ResponseEntity.ok(memberService.publishToken(authCode));
+    public ApiResponse<ApiResponse.SuccessBody<Token>> publishToken(@RequestParam String authCode) {
+        return ApiResponseGenerator.success(memberService.publishToken(authCode), HttpStatus.OK);
     }
 }
