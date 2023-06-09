a8d17514db990e57c3440d4a1d6bfd8edc21c5f5
saint6839 <saint6839@gmail.com>
2023-06-10 00:17:18 +0900

    OVG-22 feat : 인증되어 security context에 저장된 사용자의 id를 꺼내올 수 있는 기능 추가

---
+++ src/main/java/jungle/ovengers/config/security/AuditorHolder.java
@@ -0,0 +1,20 @@
+package jungle.ovengers.config.security;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@UtilityClass
+public class AuditorHolder {
+
+    public static Long get() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null
+                || !authentication.isAuthenticated()
+                || authentication.getPrincipal().equals("anonymousUser")) {
+            return 0L;
+        }
+
+        return Long.valueOf(String.valueOf(authentication.getPrincipal()));
+    }
+}
