223ad3968d303e8a015c45a44ef0c1acae4611e6
saint6839 <saint6839@gmail.com>
2023-06-09 23:27:15 +0900

    OVG-22 feat : 인가 코드를 받아 서버 jwt 토큰으로 변환해주는 컨트롤러 메서드 추가

--- src/main/java/jungle/ovengers/controller/MemberController.java
+++ src/main/java/jungle/ovengers/controller/MemberController.java
@@ -1,25 +1,28 @@
 package jungle.ovengers.controller;
 
+import jungle.ovengers.model.response.Token;
+import jungle.ovengers.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
 @Slf4j
 public class MemberController {
 
-    @GetMapping("/")
-    public String home() {
-        return "success";
+    private final MemberService memberService;
+
+    @GetMapping("/kakao")
+    public String kakaoCallback(@RequestParam String code) {
+        System.out.println(code);
+        return code;
     }
 
-    @GetMapping("/oauth/kakao")
-    public void test(@RequestParam String code) {
-        System.out.println("테스트 : " + code);
+    @PostMapping("/tokens")
+    public ResponseEntity<Token> publishToken(@RequestParam String authCode) {
+        return ResponseEntity.ok(memberService.publishToken(authCode));
     }
 }
