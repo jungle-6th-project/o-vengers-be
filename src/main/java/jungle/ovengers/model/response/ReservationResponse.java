c35cadb993cd5f5b4c79adbf66398bb8215362ec
saint6839 <saint6839@gmail.com>
2023-06-10 20:58:20 +0900

    OVG-20 feat : reservation 관련 rest api spec

---
+++ src/main/java/jungle/ovengers/model/response/ReservationResponse.java
@@ -0,0 +1,12 @@
+package jungle.ovengers.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationResponse {
+    private Long reservationId;
+}
