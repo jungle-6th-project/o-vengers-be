665d02952f66f537ccaa12d0585b56af8c63ff5c
saint6839 <saint6839@gmail.com>
2023-06-12 14:32:38 +0900

    OVG-29 feat : 예약 관련 service 클래스 추가

--- src/main/java/jungle/ovengers/controller/ReservationStompController.java
+++ src/main/java/jungle/ovengers/controller/ReservationStompController.java
@@ -1,5 +1,7 @@
 package jungle.ovengers.controller;
 
+import jungle.ovengers.model.request.ReservationRequest;
+import jungle.ovengers.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationStompController {
 
     private final SimpMessagingTemplate template;
+    private final ReservationService reservationService;
 
     @MessageMapping("/create")
-    public void test(String message) {
-        System.out.println(message);
+    public void test(ReservationRequest request) {
+        System.out.println(request.getFrom());
+        System.out.println(request.getTo());
+        System.out.println(request.getGroupId());
         System.out.println("호출됨");
-        template.convertAndSend("/topic/test", "수신 : " + message);
+        template.convertAndSend("/topic/test", "수신 : " + request.getFrom());
     }
 }
