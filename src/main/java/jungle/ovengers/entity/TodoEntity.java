7425373f73672af2eeb0478e65e84dae66088fff
saint6839 <saint6839@gmail.com>
2023-06-12 15:47:18 +0900

    OVG-33 feat : TodoEntity 추가

---
+++ src/main/java/jungle/ovengers/entity/TodoEntity.java
@@ -0,0 +1,53 @@
+package jungle.ovengers.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private MemberEntity memberEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private GroupEntity groupEntity;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column
+    private boolean isDone;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public void setMemberEntity(MemberEntity memberEntity) {
+        if (this.memberEntity != null) {
+            this.memberEntity.getTodos()
+                             .remove(this);
+        }
+        this.memberEntity = memberEntity;
+        memberEntity.getTodos().add(this);
+    }
+}
