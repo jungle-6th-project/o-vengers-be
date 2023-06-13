6b71203d8d1cc1e38f23d4c6537f39818c06d6c1
saint6839 <saint6839@gmail.com>
2023-06-13 23:21:06 +0900

    OVG-41 feat : Builder 패턴 적용

--- src/main/java/jungle/ovengers/model/response/TodoResponse.java
+++ src/main/java/jungle/ovengers/model/response/TodoResponse.java
@@ -1,15 +1,16 @@
 package jungle.ovengers.model.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class TodoResponse {
     private Long todoId;
     private Long groupId;
-    private String group;
     private String content;
 }
