5bb0d7b1e75515433dff410792cd78fd40885a89
saint6839 <saint6839@gmail.com>
2023-06-13 14:53:41 +0900

    OVG-38 feat : 사용자가 속해있는 그룹을 조회하는 기능 추가

--- src/test/java/jungle/ovengers/service/GroupServiceTest.java
+++ src/test/java/jungle/ovengers/service/GroupServiceTest.java
@@ -3,6 +3,7 @@ package jungle.ovengers.service;
 import jungle.ovengers.config.security.AuditorHolder;
 import jungle.ovengers.entity.GroupEntity;
 import jungle.ovengers.entity.MemberEntity;
+import jungle.ovengers.entity.MemberGroupEntity;
 import jungle.ovengers.model.request.GroupAddRequest;
 import jungle.ovengers.model.response.GroupResponse;
 import jungle.ovengers.repository.GroupRepository;
@@ -16,9 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -80,13 +79,13 @@ class GroupServiceTest {
         //given
         Long memberId = 1L;
         GroupEntity groupEntity = GroupEntity.builder()
-                                                  .id(1L)
-                                                  .ownerId(memberId)
-                                                  .path("path")
-                                                  .groupName("groupName")
-                                                  .isSecret(false)
-                                                  .createdAt(String.valueOf(LocalDateTime.now()))
-                                                  .build();
+                                             .id(1L)
+                                             .ownerId(memberId)
+                                             .path("path")
+                                             .groupName("groupName")
+                                             .isSecret(false)
+                                             .createdAt(String.valueOf(LocalDateTime.now()))
+                                             .build();
         List<GroupEntity> mockGroupEntities = Arrays.asList(groupEntity);
 
         when(groupRepository.findAll()).thenReturn(mockGroupEntities);
@@ -101,5 +100,46 @@ class GroupServiceTest {
                          .getGroupName()).isEqualTo(groupEntity.getGroupName());
         assertThat(result.get(0)
                          .isSecret()).isEqualTo(groupEntity.isSecret());
+    }
+
+    @DisplayName("사용자가 속한 그룹 목록이 잘 불러와지는지 테스트")
+    @Test
+    public void testGetMemberGroups() {
+        //given
+        Long memberId = 1L;
+        GroupEntity groupEntity = GroupEntity.builder()
+                                             .id(1L)
+                                             .ownerId(memberId)
+                                             .path("path")
+                                             .groupName("groupName")
+                                             .isSecret(false)
+                                             .createdAt(String.valueOf(LocalDateTime.now()))
+                                             .build();
+        MemberEntity memberEntity = MemberEntity.builder()
+                                                .id(memberId)
+                                                .email("email")
+                                                .profile("profile")
+                                                .name("name")
+                                                .build();
+        MemberGroupEntity memberGroupEntity = MemberGroupEntity.builder()
+                                                               .memberId(memberId)
+                                                               .groupId(groupEntity.getId())
+                                                               .id(memberId + 1)
+                                                               .build();
+        when(auditorHolder.get()).thenReturn(memberId);
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(memberEntity));
+        when(memberGroupRepository.findByMemberId(memberId)).thenReturn(Collections.singletonList(memberGroupEntity));
+        when(groupRepository.findById(groupEntity.getId())).thenReturn(Optional.of(groupEntity));
+
+        //when
+        List<GroupResponse> results = groupService.getMemberGroups();
+
+        //then
+        assertThat(results.get(0)
+                          .getGroupName()).isEqualTo(groupEntity.getGroupName());
+        assertThat(results.get(0)
+                          .getGroupId()).isEqualTo(groupEntity.getId());
+        assertThat(results.get(0)
+                          .isSecret()).isEqualTo(groupEntity.isSecret());
     }
 }
