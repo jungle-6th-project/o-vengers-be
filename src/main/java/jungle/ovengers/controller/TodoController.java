fc460631aa748296c22d50334d5d654f3335357a
saint6839 <saint6839@gmail.com>
2023-06-13 23:23:34 +0900

    OVG-41 feat : Todo 생성 메서드 호출

--- src/main/java/jungle/ovengers/controller/TodoController.java
+++ src/main/java/jungle/ovengers/controller/TodoController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import jungle.ovengers.model.request.TodoRequest;
 import jungle.ovengers.model.response.TodoResponse;
+import jungle.ovengers.service.TodoService;
 import jungle.ovengers.support.ApiResponse;
 import jungle.ovengers.support.ApiResponseGenerator;
 import jungle.ovengers.support.MessageCode;
@@ -22,6 +23,8 @@ import java.util.List;
 @Slf4j
 public class TodoController {
 
+    private final TodoService todoService;
+
     @ApiOperation(value = "그룹 Todo 조회")
     @ApiImplicitParam(name = "Authorization", value = "JWT token", required = true, dataTypeClass = String.class, paramType = "header")
     @GetMapping
@@ -34,14 +37,14 @@ public class TodoController {
     @ApiImplicitParam(name = "Authorization", value = "JWT token", required = true, dataTypeClass = String.class, paramType = "header")
     @PostMapping("/{groupId}")
     public ApiResponse<ApiResponse.SuccessBody<TodoResponse>> add(@PathVariable Long groupId, @RequestBody TodoRequest request) {
-        return ApiResponseGenerator.success(new TodoResponse(1L, 2L, "group", "content"), HttpStatus.CREATED, MessageCode.RESOURCE_CREATED);
+        return ApiResponseGenerator.success(todoService.generateTodo(groupId, request), HttpStatus.CREATED, MessageCode.RESOURCE_CREATED);
     }
 
     @ApiOperation(value = "그룹 Todo 수정 (내용 수정 or 완료 체크)")
     @ApiImplicitParam(name = "Authorization", value = "JWT token", required = true, dataTypeClass = String.class, paramType = "header")
     @PutMapping("/{groupId}/{todoId}")
     public ApiResponse<ApiResponse.SuccessBody<TodoResponse>> edit(@PathVariable Long groupId, @PathVariable Long todoId, @RequestBody TodoRequest request) {
-        return ApiResponseGenerator.success(new TodoResponse(1L, 2L, "group", "content"), HttpStatus.OK, MessageCode.SUCCESS);
+        return ApiResponseGenerator.success(new TodoResponse(1L, 2L, "content"), HttpStatus.OK, MessageCode.SUCCESS);
     }
 
     @ApiOperation(value = "그룹 Todo 삭제")
