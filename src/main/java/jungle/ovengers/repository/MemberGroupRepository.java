05a84628d84a271eae3ece1655f829d3d3a750bc
saint6839 <saint6839@gmail.com>
2023-06-13 13:49:02 +0900

    OVG-34 feat : Repository 추가

---
+++ src/main/java/jungle/ovengers/repository/MemberGroupRepository.java
@@ -0,0 +1,7 @@
+package jungle.ovengers.repository;
+
+import jungle.ovengers.entity.MemberGroupEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberGroupRepository extends JpaRepository<MemberGroupEntity, Long> {
+}
