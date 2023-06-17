c95739c03c164ba058331b724f77c6795d6c1096
saint6839 <saint6839@gmail.com>
2023-06-17 23:00:35 +0900

    refactor : api spec 수정

--- src/main/java/jungle/ovengers/controller/StudyHistoryController.java
+++ src/main/java/jungle/ovengers/controller/StudyHistoryController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/histories")
+@RequestMapping("/api/v1/study-histories")
 @Slf4j
 public class StudyHistoryController {
 
