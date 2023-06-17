83ec0db486df239531fd407c3f42084e38a93f52
saint6839 <saint6839@gmail.com>
2023-06-17 19:22:15 +0900

    OVG-68 feat : 예약방 생성시 유효하지 않은 요청 시간이 입력될 경우 예외 발생하도록 구현

--- src/main/java/jungle/ovengers/service/RoomStompService.java
+++ src/main/java/jungle/ovengers/service/RoomStompService.java
@@ -15,13 +15,11 @@ import jungle.ovengers.repository.MemberRoomRepository;
 import jungle.ovengers.repository.RoomRepository;
 import jungle.ovengers.support.converter.MemberRoomConverter;
 import jungle.ovengers.support.converter.RoomConverter;
+import jungle.ovengers.support.validator.RoomValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -35,6 +33,8 @@ public class RoomStompService {
     private final MemberRoomRepository memberRoomRepository;
 
     public RoomResponse generateRoom(Long memberId, RoomAddRequest request) {
+        RoomValidator.validateIfRoomTimeAfterNow(request);
+
         MemberEntity memberEntity = memberRepository.findById(memberId)
                                                     .orElseThrow(() -> new MemberNotFoundException(memberId));
 
--- src/main/java/jungle/ovengers/support/validator/RoomValidator.java
+++ src/main/java/jungle/ovengers/support/validator/RoomValidator.java
@@ -3,18 +3,30 @@ package jungle.ovengers.support.validator;
 import jungle.ovengers.model.request.RoomAddRequest;
 import lombok.experimental.UtilityClass;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @UtilityClass
 public final class RoomValidator {
+
+    private final int INTERVAL_MINUTES = 25;
     public static void validateIfRoomTimeAfterNow(RoomAddRequest request) {
         if (isInvalidTime(request)) {
-            throw new IllegalArgumentException("현재 시각 이전의 예약은 생성할 수 없습니다.");
+            throw new IllegalArgumentException("유효하지 않은 요청입니다.");
         }
     }
 
     private static boolean isInvalidTime(RoomAddRequest request) {
+        return isLateTime(request) || isInvalidTimeInterval(request);
+    }
+
+    private static boolean isLateTime(RoomAddRequest request) {
         return LocalDateTime.now()
                             .isAfter(request.getEndTime());
+    }
+
+    private static boolean isInvalidTimeInterval(RoomAddRequest request) {
+        return !Duration.between(request.getStartTime(), request.getEndTime())
+                       .equals(Duration.ofMinutes(INTERVAL_MINUTES));
     }
 }
