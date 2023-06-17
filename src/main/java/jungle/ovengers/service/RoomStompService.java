05342f391ed32b95c69294f5735a9a65822f8d74
saint6839 <saint6839@gmail.com>
2023-06-17 22:07:58 +0900

    OVG-71 fix : room 삭제시 데이터 startTime만 내리도록 변경

--- src/main/java/jungle/ovengers/service/RoomStompService.java
+++ src/main/java/jungle/ovengers/service/RoomStompService.java
@@ -71,7 +71,7 @@ public class RoomStompService {
         roomEntity.removeProfile(memberEntity.getProfile());
         if (!memberRoomRepository.existsByRoomIdAndDeletedFalse(request.getRoomId())) {
             roomEntity.delete();
-            return new RoomResponse(roomEntity.getId(), null, null, null);
+            return new RoomResponse(null, roomEntity.getStartTime(), null, null);
         }
         return RoomConverter.from(roomEntity);
     }
