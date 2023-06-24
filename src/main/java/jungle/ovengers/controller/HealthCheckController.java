ef25c61fe0e7caf8bd9f35ee084e2187bd4fc265
saint6839 <saint6839@gmail.com>
2023-06-24 17:30:16 +0900

    OVG-156 fix : HealthCheckController rest api spec에 반영하지 않도록 수정

--- src/main/java/jungle/ovengers/controller/HealthCheckController.java
+++ src/main/java/jungle/ovengers/controller/HealthCheckController.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/health")
+@ApiIgnore
 @Slf4j
 public class HealthCheckController {
 
