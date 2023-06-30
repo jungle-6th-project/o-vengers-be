4714b4cbb794886e45b29e9e5c0f953f4c10572d
saint6839 <saint6839@gmail.com>
2023-07-01 00:11:13 +0900

    OVG-136 chore :

--- src/main/java/jungle/ovengers/service/ClientService.java
+++ src/main/java/jungle/ovengers/service/ClientService.java
@@ -1,6 +1,5 @@
 package jungle.ovengers.service;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import jungle.ovengers.config.security.AuditorHolder;
 import jungle.ovengers.entity.ClientEntity;
 import jungle.ovengers.entity.MemberEntity;
@@ -11,12 +10,8 @@ import jungle.ovengers.repository.ClientRepository;
 import jungle.ovengers.repository.MemberRepository;
 import jungle.ovengers.support.converter.ClientConverter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.io.IOException;
-import java.util.List;
 
 @Transactional
 @RequiredArgsConstructor
@@ -42,12 +37,4 @@ public class ClientService {
         clientEntity.updateFcmToken(request.getFcmToken());
         return ClientConverter.from(clientEntity);
     }
-
-//    public String getAccessToken() throws IOException {
-//        String path = "o-vengers-firebase-adminsdk-gpcsd-dc4c8a5963.json";
-//        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ClassPathResource(path).getInputStream())
-//                                                    .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
-//
-//
-//    }
 }
